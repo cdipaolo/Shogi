21ed3a81afed477a88fba021d6ef14a2e1d282ec
Conner DiPaolo <cdipaolo96@gmail.com>
2015-01-18 17:00:15 -0800

    Initial Commit

---
+++ ShogiKing/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  ShogiKing
+//
+
+//  Copyright (c) 2015 Conner DiPaolo. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ ShogiKing/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  ShogiKing
+//
+
+//  Copyright (c) 2015 Conner DiPaolo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
