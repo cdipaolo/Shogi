448e4d335055dc7f17135fdb38d771fb2ae83404
Conner DiPaolo <cdipaolo96@gmail.com>
2015-01-18 20:27:20 -0800

    Started bitboard representation of Shogi Board

--- ShogiKing/ShogiBoard.h
+++ ShogiKing/ShogiBoard.h
@@ -10,4 +10,32 @@
 
 @interface ShogiBoard : NSObject
 
+
+// players pieces bitboards
+@property (nonatomic) int64_t pawn;
+@property (nonatomic) int64_t knight;
+@property (nonatomic) int64_t lance;
+@property (nonatomic) int64_t silver;
+@property (nonatomic) int64_t gold;
+@property (nonatomic) int64_t king;
+@property (nonatomic) int64_t bishop;
+@property (nonatomic) int64_t rook;
+
+// enemy pieces bitboards
+@property (nonatomic) int64_t pawnE;
+@property (nonatomic) int64_t knightE;
+@property (nonatomic) int64_t lanceE;
+@property (nonatomic) int64_t silverE;
+@property (nonatomic) int64_t goldE;
+@property (nonatomic) int64_t kingE;
+@property (nonatomic) int64_t bishopE;
+@property (nonatomic) int64_t rookE;
+
+
+- (int64_t) bitboardFromArray:(NSArray*)array forPiece:(NSString* )piece;
+- (id) initWithArray: (NSArray*) pieces;
+- (id) init;
++ (void) initialize;
+
+
 @end
--- ShogiKing/ShogiBoard.m
+++ ShogiKing/ShogiBoard.m
@@ -10,4 +10,64 @@
 
 @implementation ShogiBoard
 
+NSArray* _defaultPieces;
+
+-(int64_t) bitboardFromArray:(NSArray* )array forPiece:(NSString* )piece {
+    int64_t bitboard = 0b0;
+    for (int i=0; i<63; ++i) {
+        bitboard |= [[array objectAtIndex:i] isEqualToString:piece] ? 0b1<<(63-i) : 0b0;
+    }
+    
+    return bitboard;
+}
+
+// initialize a Shogi Board representation based on an array list of the pieces
+-(id) initWithArray: (NSArray*) pieces {
+    self = [super init];
+    
+    if (self){
+        
+        self.pawn = [self bitboardFromArray:pieces forPiece:@"P"];
+        self.knight = [self bitboardFromArray:pieces forPiece:@"N"];
+        self.lance = [self bitboardFromArray:pieces forPiece:@"L"];
+        self.silver = [self bitboardFromArray:pieces forPiece:@"S"];
+        self.gold = [self bitboardFromArray:pieces forPiece:@"G"];
+        self.king = [self bitboardFromArray:pieces forPiece:@"K"];
+        self.bishop = [self bitboardFromArray:pieces forPiece:@"B"];
+        self.rook = [self bitboardFromArray:pieces forPiece:@"R"];
+        
+        self.pawnE = [self bitboardFromArray:pieces forPiece:@"p"];
+        self.knightE = [self bitboardFromArray:pieces forPiece:@"n"];
+        self.lanceE = [self bitboardFromArray:pieces forPiece:@"l"];
+        self.silverE = [self bitboardFromArray:pieces forPiece:@"s"];
+        self.goldE = [self bitboardFromArray:pieces forPiece:@"g"];
+        self.kingE = [self bitboardFromArray:pieces forPiece:@"k"];
+        self.bishopE = [self bitboardFromArray:pieces forPiece:@"b"];
+        self.rookE = [self bitboardFromArray:pieces forPiece:@"r"];
+        
+        
+    }
+    return self;
+}
+
+// init Shogi Board with defualt starting pieces
+-(id) init {
+    return [self initWithArray: _defaultPieces];
+}
+
+// set default pieces
++(void) initialize{
+    if (self == [ShogiBoard class]) {
+        _defaultPieces = @[@"l",@"n",@"s",@"g",@"k",@"g",@"s",@"n",@"l",
+                           @" ",@"r",@" ",@" ",@" ",@" ",@" ",@"b",@" ",
+                           @"p",@"p",@"p",@"p",@"p",@"p",@"p",@"p",@"p",
+                           @" ",@" ",@" ",@" ",@" ",@" ",@" ",@" ",@" ",
+                           @" ",@" ",@" ",@" ",@" ",@" ",@" ",@" ",@" ",
+                           @" ",@" ",@" ",@" ",@" ",@" ",@" ",@" ",@" ",
+                           @"P",@"P",@"P",@"P",@"P",@"P",@"P",@"P",@"P",
+                           @" ",@"B",@" ",@" ",@" ",@" ",@" ",@"R",@" ",
+                           @"L",@"N",@"S",@"G",@"K",@"G",@"S",@"N",@"L"];
+    }
+}
+
 @end
